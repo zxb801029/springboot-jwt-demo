559069f01a8ad567ce6ff9538436f4e2238a7712
zhangxb01 <zhangxb01@zhangxb0101.home.langchao.com>
2019-03-13 17:02:56 +0800

    update

---
+++ src/main/java/cn/echisan/springbootjwtdemo/SpringbootJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.echisan.springbootjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtDemoApplication {
+
+	//https://blog.csdn.net/ech13an/article/details/80779973
+	public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtDemoApplication.class, args);
+    }
+}
